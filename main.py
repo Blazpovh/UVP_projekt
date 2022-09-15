7e7d17da61a159dd5c9f48b70f0465c3285ad00a
Blaz Povh <bp1599@student.uni-lj.si>
2022-09-15 19:03:38 +0200

    dodal prijavo, registracijo...

--- main.py
+++ main.py
@@ -1,33 +1,88 @@
-# Tukaj se bo nahajala moja glavna koda za projekt
+# Tu bom uvozil potrebne stvari
+import json
+import random
+import hashlib
 
 
-class Banka:
 
-    def __init__(self, zacetno_stanje):
-        self.zacetno_stanje = zacetno_stanje
+# Od tu naprej pa bo koda
 
-    def __str__(self):
-        return f'Stanje na računu: {self.zacetno_stanje}'
+class Uporabnik:
 
-    def __repr__(self):
-        return f'Račun({self.zacetno_stanje})'
+    def __init__(self, uporabnisko_ime, geslo_zasifrirano, model):
+        self.uporabnisko_ime = uporabnisko_ime
+        self.geslo_zasifrirano = geslo_zasifrirano
+        self.model = model
 
-    def dvig(self, koliko):
-        if koliko >= 0:
-            if koliko < self.zacetno_stanje:
-                self.zacetno_stanje -= koliko
-                return True
-            else:
-                return False
-    
-    def polog(self, koliko):
-        if koliko >= 0:
-            self.zacetno_stanje += koliko
-        return self.zacetno_stanje
 
-def prenesi(racun1, racun2, koliko):
-    if racun1.dvig(koliko):
-        racun2.polog(koliko)
-        return True
-    return False
-            
+    @staticmethod
+    def prijava(uporabnisko_ime, geslo_z_besedo):
+        uporabnik = Uporabnik.preberi_datoteko(uporabnisko_ime)
+        if uporabnik is None:
+            raise ValueError("Uporabniško ime ne obstaja")
+        elif uporabnik.preveri_geslo(geslo_z_besedo):
+            return uporabnik
+        else:
+            raise ValueError("Geslo ni pravilno")
+
+
+    @staticmethod
+    def registracija(uporabnisko_ime, geslo_z_besedo):
+        if Uporabnik.preberi_datoteko(uporabnisko_ime) is not None:
+            raise ValueError("Uporabniško ime je že zasedeno")
+        else:
+            geslo_zasifrirano = Uporabnik.zasifriraj_geslo(geslo_z_besedo)
+            uporabnik = Uporabnik(uporabnisko_ime, geslo_zasifrirano, Model())
+            uporabnik.shrani_datoteko()
+            return uporabnik
+
+    @staticmethod
+    def zasifriraj_geslo(geslo_z_besedo, sol=None):
+        if sol is not None:
+            sol = str(random.getrandbits(32))
+        sposojeno_geslo = sol + geslo_z_besedo
+        h = hashlib.blake2b()
+        h.update(sposojeno_geslo.encode(encoding="utf-8"))
+        return f"{sol}${h.hexdigest()}"
+
+    def v_slovar(self):
+        return {
+            "uporabnisko_ime": self.uporabnisko_ime,
+            "geslo_zasifirano": self.geslo_zasifrirano,
+            "model": self.model.v_slovar(),
+        }
+
+    @staticmethod
+    def ime_uporabnikove_datoteke(uporabnisko_ime):
+        return f"{uporabnisko_ime}.json"
+
+
+    def shrani_datoteko(self):
+        with open(Uporabnik.ime_uporabnikove_datoteke(self.uporabnisko_ime), "w", encoding="utf-8") as datoteka:
+            json.dump(self.v_slovar(), datoteka, ensure_ascii=False, indent=4)
+
+    def preveri_geslo(self, geslo_z_besedo):
+        sol, _ = self.geslo_zasifrirano.split("$")
+        return self.geslo_zasifrirano == Uporabnik.zasifriraj_geslo(geslo_z_besedo, sol)
+
+    @staticmethod
+    def iz_slovarja(slovar):
+        uporabnisko_ime = slovar["uporabnisko_ime"]
+        geslo_zasifrirano = slovar["geslo_zasifrirano"]
+        model = Model.iz_slovarja(slovar["model"])
+        return Uporabnik(uporabnisko_ime, geslo_zasifrirano, model)
+
+    @staticmethod
+    def preberi_datoteko(uporabnisko_ime):
+        try:
+            with open(Uporabnik.ime_uporabnikove_datoteke(uporabnisko_ime), "r", encoding="utf-8") as datoteka:
+                slovar = json.load(datoteka)
+                return Uporabnik.iz_slovarja(slovar)
+        except FileNotFoundError:
+            return None
+
+
+
+
+
+
